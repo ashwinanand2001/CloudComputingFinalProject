189ffa63904a60cf7b4a1ce6479125d505fe968f
ashwinanand01 <ashand01@gmail.com>
2023-12-18 15:45:26 -0500

    Removing Attempted Web Application

--- cloudcomputingmovierecommedproj/movierecommendapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MovierecommendappConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "movierecommendapp"
--- cloudcomputingmovierecommedproj/movierecommendapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Movie(models.Model):
-    title = models.CharFiels(max_length=255)
-
-class UserSearch(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    search_query = models.CharField(max_length=255)
